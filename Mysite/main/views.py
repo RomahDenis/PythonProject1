10bbaf000ef6853f63e37862ad1c36b7e3a8262e
RomahDenis <denis.romah77@gmail.com>
2025-05-07 19:31:11 +0300

    file 4

---
+++ Mysite/main/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http inport HttpResponse
+
+def index(request):
+    return HttpResponce("<h4>Перевірки роботи</h4>")
+
+def about(request):
+    return HttpResponce("<h4>Зкщ тфі</h4>")
+
