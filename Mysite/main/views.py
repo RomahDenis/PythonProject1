57ee1fad351b2e8fbd76b5c70f6a1b6fb04c3b73
RomahDenis <denis.romah77@gmail.com>
2025-05-12 21:26:54 +0300

    file 5

--- Mysite/main/views.py
+++ Mysite/main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h4>Перевірки роботи</h4>")
+    return render(request, 'main/index.html')
 
 def about(request):
     return HttpResponse("<h4>Pro Nas</h4>")
